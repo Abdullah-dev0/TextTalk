32fc698826ed691daeb33618c83274d2a1f3c60b
Abdullah-142 <abdulah14200@gmail.com>
2025-03-02 17:18:01 +0500

    Add Vercel Analytics to the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Providers from "@/components/Providers";
 import { cn, constructMetadata } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "simplebar-react/dist/simplebar.min.css";
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					<body className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>
 						<Toaster duration={3000} richColors />
 						{children}
+						<Analytics />
 					</body>
 				</Providers>
 			</html>
